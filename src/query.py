c22c75f37a54906d9ab62df91ae552feed7c4bd7
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2011-01-29 12:54:36 -0300

    Retored query.py xD

---
+++ src/query.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+from parser import Parser
+import MySQLdb
+import shutil
+import sys
+
+class Query():
+
+    def __init__(self, args):
+        self.sqlsentence = ('INSERT INTO %s(%s, %s, %s, %s, %s)'
+                            'VALUES(%s, %s, %s, %s, %s)')
+        self.path = args[1]
+
+    def connect_to_db(self, serverdata):
+        self.datab = MySQLdb.connect(serverdata[0], serverdata[1],
+            serverdata[2], serverdata[3])
+        self.cursor = self.datab.cursor()
+        return True
+
+    def insert_to_db(self, values):
+        self.cursor.execute(self.sqlsentence % values)
+        return True
+
+    def get_data_from_config(self):
+        parse = Parser()
+        parse.generate_values()
+        self.serverdata = parse.get_serverdata_for_connection()
+        self.values = parse.get_values_for_query()
+        return True
+
+    def get_serverdata(self):
+        return self.serverdata
+
+    def get_values(self):
+        return self.values
+
+    def get_reports(self):
+        return self.reports
+
+    def format_data_to_insert(self):
+        file = open(self.path, 'r')
+        data = [line.strip() for line in file.readlines()]
+        self.reports = []
+        data = [line.strip() for line in data]
+
+        horaenvio = data[0].split()[1].replace(':','')
+        horarecepcion = data[1].split()[1].replace(':','')
+        telefono = data[2][-10:]
+
+        body = data[3]
+        normalbody = "".join([char if char.isdigit() else ";"
+            for char in body])
+        campos = [campo for campo in normalbody.split(";")
+            if campo]
+
+        print("### " + "".join(campos))
+        print campos
+        print self.values
+        
+        if len(campos) > 1:
+            planilla = campos[0]
+            ordenes = campos[1:]
+            for orden in ordenes:
+                self.reports.append(self.values + (telefono, planilla, orden, horaenvio, horarecepcion))
+            return True
+        else:
+            print("  xxx %s %s %s" % (horaenvio, telefono, data[3]))
+
+
+def main():
+    db = Query(sys.argv)
+    db.get_data_from_config()
+    db.format_data_to_insert()
+    print db.get_reports()
+    try:
+        db.connect_to_db(db.get_serverdata())
+    except:
+        print("    XX Está la base de datos online?")
+        shutil.move(sys.argv[1], "to_db")
+    
+    for report in db.get_reports():
+        db.insert_to_db(report)
+
+
+if __name__ == '__main__':
+    exit(main())
