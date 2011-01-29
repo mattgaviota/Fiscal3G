8c50fff4fc5f36d184135952d18dc381ebcb4fd6
mattgaviota <mattgaviota@gmail.com>
2011-01-29 19:57:17 -0300

    archivo malo

--- src/fiscal.sh
+++ src/fiscal.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 while true; do
     python src/fiscal.py | tee -a general.log
-    timeout 30 python src/query.py to_db/* | tee -a report.log
-    rm to_db/* 2> /dev/null 
+    timeout 300 python src/query.py to_db/* && rm to_db/* 2> /dev/null 
     sleep 5
     timestamp sleep
 done
