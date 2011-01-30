f3e36283d318eb8618518f9cf5e194b59ffa383d
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2011-01-30 12:51:25 -0300

    Last botonear script 	modified:   src/botonear.sh

--- src/botonear.sh
+++ src/botonear.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
-uniq report_archive.csv|grep "$1,"
-echo "Envió $(grep $1, report_archive.csv|wc -l) votos."
+uniq report_archive.csv | awk -v numero="$1," '$5~numero{print $0}'
+uniq report_archive.csv | awk -v numero="$1," '
+$5~numero{total++}
+END{print "Envió", total, "votos"}
+'
+
 echo "Envió $(grep $1 inbox_archive.mbox|grep From:|wc -l) mensajes."
+
 awk -v numero="$1" '
 
 BEGIN{
