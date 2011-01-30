c14aca5a58dec4194ddd157443af3db6a5d9e23d
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2011-01-30 17:14:34 -0300

    New reporter

--- src/miniresumen.sh
+++ src/miniresumen.sh
@@ -6,7 +6,7 @@ uniq report_archive.csv | awk -v numero="$1," '
     }
 
     END{
-        printf "%-05d ", total
+        printf "%05d ", total
     }
 '
 
@@ -16,7 +16,7 @@ awk -v numero="$1" '
     }
 
     END{
-        printf "%d ", total
+        printf "%05d ", total
     }
 ' inbox_archive.mbox
 
