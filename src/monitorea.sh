7d85ba445e9989c5d29fb2baf280391088786381
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2011-01-30 21:04:19 -0300

    reportes

--- src/monitorea.sh
+++ src/monitorea.sh
@@ -4,14 +4,27 @@ awk -F "," -v OFS=";" '
     /espartanos/{
         gsub("\\\|", "")
         system("sh src/miniresumen.sh " $2)
-        print $2, $1
+        print ";"$2, $1
     }
 ' contacts.csv 2>/dev/null | sort -n > reportebase.txt
 
-awk -F ";" '
-
-    {
-        print $1, $2, $3, $4
+awk -F ";" -v OFS=";" '
+    BEGIN{
+        fmstr = "%-5s\t%-4s\t%8s\t%10s\t%s\n"
+        printf fmstr, "VOTOS", "SMSs", "ULTIMO", "CELULAR", "APELLIDO NOMBRE"        
     }
+    {
+        printf fmstr, $1, $2, $3, $4, $5
+    }
+' reportebase.txt > reporte_votos.txt
 
-'
+awk -F ";" -v OFS=";" '
+    BEGIN{
+        fmstr = "%-4s\t%-5s\t%8s\t%10s\t%s\n"
+        printf fmstr, "SMSs", "VOTOS", "ULTIMO", "CELULAR", "APELLIDO NOMBRE"        
+    }
+    {
+        printf fmstr, $2, $1, $3, $4, $5
+    }
+' reportebase.txt > reporte_smss.txt
+
