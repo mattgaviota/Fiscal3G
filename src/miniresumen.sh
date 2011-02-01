5bf790b0e7f3f5fca3533b0a3bc9207cee9e5d3c
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2011-02-01 19:52:52 -0300

    Updated final reports

--- src/miniresumen.sh
+++ src/miniresumen.sh
@@ -1,42 +1,96 @@
 #!/bin/sh
 
-uniq report_archive.csv | awk -v numero="$1" '
-    $5~numero{
-        total++
-    }
+#VOTOS
+VOTOS=$(
+    uniq report_archive.csv | awk -v numero="$1" '
+        $5~numero{
+            total++
+        }
+        END{
+            printf "%d", total
+        }
+    '
+)
 
-    END{
-        printf "%d;", total
-    }
-'
+#SMSs
+SMS=$(
+    awk -v numero="$1" '
+        /^From:/ && $2 ~ numero {
+            total++
+        }
+        END{
+            printf "%d", total
+        }
+    ' inbox_archive.mbox
+)
 
-awk -v numero="$1" '
-    /^From:/ && $2 ~ numero {
-        total++
-    }
+#VOTOS/SMSs
+VOTOSPORSMS=$(
+    echo $VOTOS / $SMS|bc -l
+)
 
-    END{
-        printf "%d;", total
-    }
-' inbox_archive.mbox
-
-awk -v numero="$1" '
-
-    BEGIN{
-        hora = "000000"
-    }
-
-    $5~numero{
-        sub(",", "", $(NF - 1))
-        if (hora < $(NF -1))
-            hora = $(NF - 1)
-    }
-
-    END{
-        shora = substr(hora, 1, 2)
-        sminu = substr(hora, 3, 2)
-        ssegu = substr(hora, 5, 2)
-        printf "%02d:%02d:%02d", shora, sminu, ssegu
-    }
-
+#PRIMERO
+PRIMERO=$(
+    awk -v numero="$1" '
+        BEGIN{
+            hora = "999999"
+        }
+        $5~numero{
+            sub(",", "", $(NF - 1))
+            if (hora > $(NF -1))
+                hora = $(NF - 1)
+        }
+        END{
+            shora = substr(hora, 1, 2)
+            sminu = substr(hora, 3, 2)
+            ssegu = substr(hora, 5, 2)
+            printf "%02d:%02d:%02d", shora, sminu, ssegu
+        }
 ' report_archive.csv
+)
+
+#ULTIMO
+ULTIMO=$(
+    awk -v numero="$1" '
+        BEGIN{
+            hora = "000000"
+        }
+        $5~numero{
+            sub(",", "", $(NF - 1))
+            if (hora < $(NF -1))
+                hora = $(NF - 1)
+        }
+        END{
+            shora = substr(hora, 1, 2)
+            sminu = substr(hora, 3, 2)
+            ssegu = substr(hora, 5, 2)
+            printf "%02d:%02d:%02d", shora, sminu, ssegu
+        }
+    ' report_archive.csv
+)
+
+#SEGSPORSMS
+SEGSPORSMS=$(
+    awk -v primero="$PRIMERO" -v ultimo="$ULTIMO" -v sms="$SMS" '
+        BEGIN{
+            split(primero, aprimero, ":")
+            split(ultimo, aultimo, ":")
+            time = aprimero[1] * 3600 + aprimero[2] * 60 + aprimero[3]
+            print time / sms
+        }
+    '
+)
+
+#SEGSPORVOTO
+SEGSPORVOTO=$(
+    awk -v primero="$PRIMERO" -v ultimo="$ULTIMO" -v votos="$VOTOS" '
+        BEGIN{
+            split(primero, aprimero, ":")
+            split(ultimo, aultimo, ":")
+            time = aprimero[1] * 3600 + aprimero[2] * 60 + aprimero[3]
+            print time / votos
+        }
+    '
+)
+
+echo "$VOTOS;$SMS;$VOTOSPORSMS;$PRIMERO;$ULTIMO;$SEGSPORSMS;$SEGSPORVOTO"
--- src/monitorea.sh
+++ src/monitorea.sh
@@ -7,24 +7,3 @@ awk -F "," -v OFS=";" '
         print ";"$2, $1
     }
 ' contacts.csv 2>/dev/null | sort -n > reportebase.txt
-
-awk -F ";" -v OFS=";" '
-    BEGIN{
-        fmstr = "%-5s\t%-4s\t%8s\t%10s\t%s\n"
-        printf fmstr, "VOTOS", "SMSs", "ULTIMO", "CELULAR", "APELLIDO NOMBRE"        
-    }
-    {
-        printf fmstr, $1, $2, $3, $4, $5
-    }
-' reportebase.txt > reporte_votos.txt
-
-awk -F ";" -v OFS=";" '
-    BEGIN{
-        fmstr = "%-4s\t%-5s\t%8s\t%10s\t%s\n"
-        printf fmstr, "SMSs", "VOTOS", "ULTIMO", "CELULAR", "APELLIDO NOMBRE"        
-    }
-    {
-        printf fmstr, $2, $1, $3, $4, $5
-    }
-' reportebase.txt > reporte_smss.txt
-
